1cbd5e943a22fb2900c7744b059b15f4fdc9ab14
Nomri445 <9m9vy4hkhb@privaterelay.appleid.com>
2026-07-12 13:21:06 +0530

    Update flashcards.js

--- flashcards.js
+++ flashcards.js
@@ -1,38 +1,26 @@
 let currentIndex = 0;
 
 function loadCard() {
-    const card = sentences[currentIndex];
+    englishSentence.textContent = sentences[currentIndex].english;
+    germanSentence.textContent = sentences[currentIndex].german;
 
-    document.getElementById("englishSentence").textContent = card.english;
-    document.getElementById("germanSentence").textContent = card.german;
-
-    document.getElementById("answer").style.display = "none";
+    answer.style.display = "none";
 
     document.querySelector(".progress").textContent =
         `${currentIndex + 1} / ${sentences.length}`;
 }
 
-document.getElementById("showAnswerBtn").onclick = function () {
-    document.getElementById("answer").style.display = "block";
+showAnswerBtn.onclick = function () {
+    answer.style.display = "block";
 };
 
-document.getElementById("nextBtn").onclick = function () {
+nextBtn.onclick = function () {
     currentIndex++;
-    
-    if (currentIndex >= sentences.length) {
-        currentIndex = 0;
-    }
-
     loadCard();
 };
 
-document.getElementById("prevBtn").onclick = function () {
+prevBtn.onclick = function () {
     currentIndex--;
-
-    if (currentIndex < 0) {
-        currentIndex = sentences.length - 1;
-    }
-
     loadCard();
 };
 
