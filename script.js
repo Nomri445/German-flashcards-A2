e83efaa39a6d0d9697fe525230c099905e141c51
Nomri445 <9m9vy4hkhb@privaterelay.appleid.com>
2026-07-12 12:30:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const startBtn = document.getElementById("startBtn");
+
+if (startBtn) {
+    startBtn.addEventListener("click", () => {
+        window.location.href = "flashcards.html";
+    });
+}
